93e652cad97e9ede643b97518d16838c414fe3ed
plaudat <31513800+plaudat@users.noreply.github.com>
2022-07-30 13:56:53 -0400

    GraphQL.Installed new node, express, apollo.used async in srvr.js to start apollo

--- server/server.js
+++ server/server.js
@@ -1,7 +1,52 @@
 const express = require("express");
-const app = express();
-const fileServerMiddleware = express.static("public");
-app.use("/", fileServerMiddleware);
-app.listen(3000, function () {
-  console.log("App started on port 3000");
-});
+const { ApolloServer } = require("apollo-server-express");
+
+let aboutMessage = "Issue Tracker API v1.0";
+
+const typeDefs = `
+  type Query {
+    about: String!
+  }
+  type Mutation {
+    setABoutMessage(message: String!): String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    about: () => aboutMessage,
+  },
+  Mutation: {
+    setABoutMessage,
+  },
+};
+
+function setABoutMessage(_, { message }) {
+  return (aboutMessage = message);
+}
+
+// const server = new ApolloServer({typeDefs, resolvers,});
+
+// const app = express();
+
+// app.use("/", express.static("public"));
+
+async function startApolloServer(typeDefs, resolvers) {
+  const server = new ApolloServer({ typeDefs, resolvers });
+  const app = express();
+  app.use("/", express.static("public"));
+  await server.start();
+  server.applyMiddleware({ app, path: "/graphql" });
+
+  app.listen(3000, function () {
+    console.log("App started on port 3000");
+  });
+}
+
+startApolloServer(typeDefs, resolvers);
+
+// server.applyMiddleware({ app, path: "/graphql" });
+
+// app.listen(3000, function () {
+//   console.log("App started on port 3000");
+// });
