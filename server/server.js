58374123eb76c8a3486ed7e84239951eb23a80c6
plaudat <31513800+plaudat@users.noreply.github.com>
2022-07-30 15:16:41 -0400

    The List API

--- server/server.js
+++ server/server.js
@@ -1,20 +1,36 @@
+const fs = require("fs");
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 
 let aboutMessage = "Issue Tracker API v1.0";
 
-const typeDefs = `
-  type Query {
-    about: String!
-  }
-  type Mutation {
-    setABoutMessage(message: String!): String
-  }
-`;
+const issuesDB = [
+  {
+    id: 1,
+    status: "New",
+    owner: "Ravan",
+    effort: 5,
+    created: new Date("2018-08-15"),
+    due: undefined,
+    title: "Error in console when clicking Add",
+  },
+  {
+    id: 2,
+    status: "Assigned",
+    owner: "Eddie",
+    effort: 14,
+    created: new Date("2018-08-16"),
+    due: new Date("2018-08-30"),
+    title: "Missing bottom border on panel",
+  },
+];
+
+const typeDefs = fs.readFileSync("./server/schema.graphql", "utf-8");
 
 const resolvers = {
   Query: {
     about: () => aboutMessage,
+    issueList,
   },
   Mutation: {
     setABoutMessage,
@@ -25,11 +41,9 @@ function setABoutMessage(_, { message }) {
   return (aboutMessage = message);
 }
 
-// const server = new ApolloServer({typeDefs, resolvers,});
-
-// const app = express();
-
-// app.use("/", express.static("public"));
+function issueList() {
+  return issuesDB;
+}
 
 async function startApolloServer(typeDefs, resolvers) {
   const server = new ApolloServer({ typeDefs, resolvers });
@@ -44,9 +58,3 @@ async function startApolloServer(typeDefs, resolvers) {
 }
 
 startApolloServer(typeDefs, resolvers);
-
-// server.applyMiddleware({ app, path: "/graphql" });
-
-// app.listen(3000, function () {
-//   console.log("App started on port 3000");
-// });
